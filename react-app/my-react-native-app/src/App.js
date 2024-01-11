a0d4df4b138503ad1dc329c37d2295935d04a8a0
MariLFO <mari.fer.onorio@gmail.com>
2024-01-11 11:52:08 -0300

    PizzaTranslator com reactive native

---
+++ react-app/my-react-native-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import PizzaTranslator from './PizzaTranslator';
+
+function App() {
+  return (
+    <PizzaTranslator/>
+  );
+}
+
+export default App;
