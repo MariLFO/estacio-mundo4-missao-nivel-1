4354e466e99f3a75b53fa87386c7dd93f96494b6
MariLFO <mari.fer.onorio@gmail.com>
2024-01-14 10:09:13 -0300

    Microatividade 4.1 FlatListBasics

--- react-app/my-react-native-app/src/App.js
+++ react-app/my-react-native-app/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-import ListaDinamica from './ScrollView';
+import PizzaTranslator from './PizzaTranslator';
+import ListaDinamica from './ListaDinamica';
+import FlatListBasics from './FlatListBasics';
 
 function App() {
   return (
-    <ListaDinamica/>
+    // Microatividade 2:
+    //<PizzaTranslator/>
+    // Microatividade 3:
+    //<ListaDinamica/>
+    // Microatividade 4.1:
+    <FlatListBasics/>
   );
 }
 
