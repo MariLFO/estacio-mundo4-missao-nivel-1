7c38214f7ae48e2afbe4aa08e807713e3e73555c
MariLFO <mari.fer.onorio@gmail.com>
2024-01-14 10:52:10 -0300

    Missao Pratica - etapa 2

--- react-app/meeting-app/src/Cat.jsx
+++ react-app/meeting-app/src/Cat.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
  
 const Cat = () => {
-  const name = 'Maru';
-  return <Text>Hello, I am {name}!</Text>;
+  return (
+    <View>
+      <Text>Hello, I am...</Text>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+        defaultValue="Name me!"
+      />
+    </View>
+  );
 };
-
+ 
 export default Cat;
